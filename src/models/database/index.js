e449c8176149b294520cc47ba8a2a60b599b76fd
njeri-ngigi <njery.ngigi@gmail.com>
2020-12-27 01:00:46 +0300

    seed questions and topics on start

--- src/models/database/index.js
+++ src/models/database/index.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const { CONNECTION_STRING } = require('../../environments');
+const { insertManyQuestions, findAllQuestions } = require("../../services/question");
+const { insertManyTopics, findAllTopics } = require("../../services/topic");
 
 const connectDB = async () => {
   try {
@@ -9,6 +11,20 @@ const connectDB = async () => {
       useCreateIndex: true,
     });
     console.log('Database connection successful.');
+
+    // seed questions data
+    const questions = await findAllQuestions();
+    if (questions.length < 1) {
+      await insertManyQuestions();
+      console.log('Questions data added successfully.')
+    }
+
+    // seed topics data
+    const topics = await findAllTopics();
+    if (topics.length < 1) {
+      await insertManyTopics();
+      console.log('Topics data added successfully.')
+    }
   } catch(e) {
     console.log('Database connection failed.');
     console.error(e);
